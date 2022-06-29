e3592aa4c2040b947951e052d8a1f3eccdb14777
maicabnd7647 <102701767+maicabnd7647@users.noreply.github.com>
2022-06-29 18:37:35 -0500

    Add files via upload

---
+++ aqui-main/aqui-main/cl28lqmp.js
@@ -0,0 +1,30 @@
+class Cannon {
+  constructor(x, y, width, height, angle) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.angle = angle;
+  }
+  display() {
+    // change the angle in the upward direction when the left arrow key was pressed
+    if(keyIsDown(W) && this.angle < 0.35){
+      this.angle = this.angle+ 0.02;
+
+    }
+    if(keyIsDown(S)&& this.angle > -1.45){
+      this.angle= this.angle- 0.02; 
+
+    }
+   //  changed the angle in the downward direction when the right arrow key was pressed
+
+    fill("#676e6a");
+    push();
+    translate(this.x, this.y);
+    rotate(this.angle);
+    rect(-10, -20, this.width, this.height);
+    pop();
+    arc(this.x - 30, this.y + 90, 140, 200, PI, TWO_PI);
+    noFill();
+  }
+}
